e2a7ee7786effb098af9eb146374ee3635b79a23
sumzali083 <alisummer066@icloud.com>
2026-02-27 20:58:51 +0000

    Add Transaction class with core properties (Amount, Category, Description, Date)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FinanceTracker
+{
+    class Program
+    {
+        static void Main()
+        {
+            // code here
+            Transaction transaction = new Transaction();
+            transaction.Balance();
+        }
+    }
+}
---
+++ Transaction.cs
@@ -0,0 +1,12 @@
+
+
+namespace FinanceTracker;
+
+public class Transaction{
+    public decimal Amount{get; set;}
+    public string Category{get; set;}
+    public string Description{get; set;}
+    public DateTime Date { get; set; }
+    
+//data class
+}
